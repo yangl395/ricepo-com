b9749a7cb3c987551c1392573cce00f469d45191
Hao Chen <areigna@gmail.com>
2015-04-02 13:56:06 -0400

    redirect for mobile

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,20 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  var agent = req.header('User-Agent').toLowerCase();
+  //iphone
+  if(/iphone/i.test(agent)){
+    res.redirect('https://itunes.apple.com/us/app/ricepo-chinese-food-delivery/id844835003?mt=8');
+  }
+  //android
+  else if(/android/i.test(agent)){
+    //res.sendfile(__dirname + '/public/Ricepo-release 1.1.1.apk');
+    res.redirect('https://play.google.com/store/apps/details?id=com.ricepo.app');
+  }
+  //else
+  else{
+    res.render('index', { title: 'Express' });
+  }
 });
 
 module.exports = router;
